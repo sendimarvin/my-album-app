d54a79da003b59fba163f3ec9d50f26a12771d1e
SENDIKADDIWA MARVIN <sendimarvin1@gmail.com>
2019-11-09 18:03:40 +0300

    First release of my album store app

---
+++ src/app/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Http , Response } from '@angular/http'
+// import { map } from 'rxjs/add/operator/map'; 
+import 'rxjs/Rx';
+
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable'; 
+import { Product } from './product'; 
+
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl:string = '../assets/album.json';
+  private  _productsUrl: string = '../assets/products.json';
+
+  constructor(private _http:Http) {
+
+
+  }
+
+  getAlbum (id:number):Observable<Album> {
+    return this._http.get(this._albumUrl)
+      .map(response => <Album>response.json())
+  }
+
+  getProducts  ():Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+      .map(response =>  <Product[]>response.json())
+  }
+
+}
